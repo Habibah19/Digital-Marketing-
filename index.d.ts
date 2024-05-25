adf0d4b7d19a39ba1a9a99d05222f7883aff486f
Habibah19 <166958347+Habibah19@users.noreply.github.com>
2024-05-25 05:14:49 +0300

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,62 +1,63 @@
-// Definitions by: Carlos Ballesteros Velasco <https://github.com/soywiz>
-//                 Leon Yu <https://github.com/leonyu>
-//                 BendingBender <https://github.com/BendingBender>
-//                 Maple Miao <https://github.com/mapleeit>
+import Dispatcher from'./dispatcher'
+import { setGlobalDispatcher, getGlobalDispatcher } from './global-dispatcher'
+import { setGlobalOrigin, getGlobalOrigin } from './global-origin'
+import Pool from'./pool'
+import { RedirectHandler, DecoratorHandler } from './handlers'
 
-/// <reference types="node" />
-import * as stream from 'stream';
-import * as http from 'http';
+import BalancedPool from './balanced-pool'
+import Client from'./client'
+import buildConnector from'./connector'
+import errors from'./errors'
+import Agent from'./agent'
+import MockClient from'./mock-client'
+import MockPool from'./mock-pool'
+import MockAgent from'./mock-agent'
+import mockErrors from'./mock-errors'
+import ProxyAgent from'./proxy-agent'
+import { request, pipeline, stream, connect, upgrade } from './api'
 
-export = FormData;
+export * from './cookies'
+export * from './fetch'
+export * from './file'
+export * from './filereader'
+export * from './formdata'
+export * from './diagnostics-channel'
+export * from './websocket'
+export * from './content-type'
+export * from './cache'
+export { Interceptable } from './mock-interceptor'
 
-// Extracted because @types/node doesn't export interfaces.
-interface ReadableOptions {
-  highWaterMark?: number;
-  encoding?: string;
-  objectMode?: boolean;
-  read?(this: stream.Readable, size: number): void;
-  destroy?(this: stream.Readable, error: Error | null, callback: (error: Error | null) => void): void;
-  autoDestroy?: boolean;
-}
+export { Dispatcher, BalancedPool, Pool, Client, buildConnector, errors, Agent, request, stream, pipeline, connect, upgrade, setGlobalDispatcher, getGlobalDispatcher, setGlobalOrigin, getGlobalOrigin, MockClient, MockPool, MockAgent, mockErrors, ProxyAgent, RedirectHandler, DecoratorHandler }
+export default Undici
 
-interface Options extends ReadableOptions {
-  writable?: boolean;
-  readable?: boolean;
-  dataSize?: number;
-  maxDataSize?: number;
-  pauseStreams?: boolean;
-}
-
-declare class FormData extends stream.Readable {
-  constructor(options?: Options);
-  append(key: string, value: any, options?: FormData.AppendOptions | string): void;
-  getHeaders(userHeaders?: FormData.Headers): FormData.Headers;
-  submit(
-    params: string | FormData.SubmitOptions,
-    callback?: (error: Error | null, response: http.IncomingMessage) => void
-  ): http.ClientRequest;
-  getBuffer(): Buffer;
-  setBoundary(boundary: string): void;
-  getBoundary(): string;
-  getLength(callback: (err: Error | null, length: number) => void): void;
-  getLengthSync(): number;
-  hasKnownLength(): boolean;
-}
-
-declare namespace FormData {
-  interface Headers {
-    [key: string]: any;
-  }
-
-  interface AppendOptions {
-    header?: string | Headers;
-    knownLength?: number;
-    filename?: string;
-    filepath?: string;
-    contentType?: string;
-  }
-
-  interface SubmitOptions extends http.RequestOptions {
-    protocol?: 'https:' | 'http:';
-  }
+declare namespace Undici {
+  var Dispatcher: typeof import('./dispatcher').default
+  var Pool: typeof import('./pool').default;
+  var RedirectHandler: typeof import ('./handlers').RedirectHandler
+  var DecoratorHandler: typeof import ('./handlers').DecoratorHandler
+  var createRedirectInterceptor: typeof import ('./interceptors').createRedirectInterceptor
+  var BalancedPool: typeof import('./balanced-pool').default;
+  var Client: typeof import('./client').default;
+  var buildConnector: typeof import('./connector').default;
+  var errors: typeof import('./errors').default;
+  var Agent: typeof import('./agent').default;
+  var setGlobalDispatcher: typeof import('./global-dispatcher').setGlobalDispatcher;
+  var getGlobalDispatcher: typeof import('./global-dispatcher').getGlobalDispatcher;
+  var request: typeof import('./api').request;
+  var stream: typeof import('./api').stream;
+  var pipeline: typeof import('./api').pipeline;
+  var connect: typeof import('./api').connect;
+  var upgrade: typeof import('./api').upgrade;
+  var MockClient: typeof import('./mock-client').default;
+  var MockPool: typeof import('./mock-pool').default;
+  var MockAgent: typeof import('./mock-agent').default;
+  var mockErrors: typeof import('./mock-errors').default;
+  var fetch: typeof import('./fetch').fetch;
+  var Headers: typeof import('./fetch').Headers;
+  var Response: typeof import('./fetch').Response;
+  var Request: typeof import('./fetch').Request;
+  var FormData: typeof import('./formdata').FormData;
+  var File: typeof import('./file').File;
+  var FileReader: typeof import('./filereader').FileReader;
+  var caches: typeof import('./cache').caches;
 }
